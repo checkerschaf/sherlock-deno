1d6c0cb6d0080c179fb35c45d81ec5a176398d88
checkerschaf <jan.poth1999@gmail.com>
2020-09-19 18:59:50 +0200

    WIP: add test for false positives + false negatives 🕵️

---
+++ tests/sites/all-sites.test.ts
@@ -0,0 +1,41 @@
+import { getSiteResult } from "../../src/lib/fetcher.ts";
+import { sites } from "../../sites.ts";
+import { assert } from "../testing-deps.ts";
+import { ScannerResult } from "../../src/enums.ts";
+import { Site } from "../../src/types.ts";
+
+const getClaimedUsername = (site: Site) => site.username_claimed ?? "JohnDoe";
+
+const generateRandomString = (strLength = 12) => {
+  let result = "";
+  let characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < strLength; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+const testSite = async (siteName: string) => {
+  const site = sites[siteName];
+
+  Deno.test(`site ${siteName}: a claimed username results in SUCCESS or ERROR`, async () => {
+    assert(
+      ((await getSiteResult(sites[siteName], getClaimedUsername(site)))
+        .result) !==
+        ScannerResult.NOT_FOUND,
+    );
+  });
+
+  Deno.test(`site ${siteName}: an unclaimed username results in NOT_FOUND or ERROR`, async () => {
+    assert(
+      ((await getSiteResult(sites[siteName], generateRandomString()))
+        .result) !==
+        ScannerResult.SUCCESS,
+    );
+  });
+};
+
+Object.keys(sites).map(async (siteName) => {
+  await testSite(siteName);
+});
