354ba3688e9c7f9ca6395c9ef60dbdc6e7bfb4e7
checkerschaf <jan.poth1999@gmail.com>
2020-12-15 09:38:24 +0100

    Update dependencies 🕵️

--- tests/testing-deps.ts
+++ tests/testing-deps.ts
@@ -2,10 +2,10 @@ export {
   assert,
   assertEquals,
   assertStringIncludes,
-} from "https://deno.land/std@0.79.0/testing/asserts.ts";
+} from "https://deno.land/std@0.81.0/testing/asserts.ts";
 
-export { FakeTime } from "https://deno.land/x/mock@v0.9.2/time.ts";
+export { FakeTime } from "https://deno.land/x/mock@v0.9.4/time.ts";
 
-export type { Stub } from "https://deno.land/x/mock@v0.9.2/stub.ts";
+export type { Stub } from "https://deno.land/x/mock@v0.9.4/stub.ts";
 
-export { stub } from "https://deno.land/x/mock@v0.9.2/stub.ts";
+export { stub } from "https://deno.land/x/mock@v0.9.4/stub.ts";
