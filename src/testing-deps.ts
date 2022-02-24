194460145e3ec14d1b5af63bd1f408a184d90c09
checkerschaf <git@janpoth.de>
2022-02-24 10:35:33 +0100

    bump dependencies 🕵️

--- src/testing-deps.ts
+++ src/testing-deps.ts
@@ -3,7 +3,7 @@ export {
   assertEquals,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.125.0/testing/asserts.ts";
+} from "https://deno.land/std@0.126.0/testing/asserts.ts";
 
 export { FakeTime } from "https://deno.land/x/mock@0.13.0/time.ts";
 
